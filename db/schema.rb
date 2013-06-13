8065c2337596141c4f290761f88a33017c97087d
Martin Burke <mahtyb@gmail.com>
2013-06-12 17:12:14 -0700

    Add User Management

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612045534) do
+ActiveRecord::Schema.define(:version => 20130612221814) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130612045534) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
