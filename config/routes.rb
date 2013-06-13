578de1f06b07c6585b15be6dd842093f3d00655a
Martin Burke <mahtyb@gmail.com>
2013-06-13 16:20:54 -0700

    Seed database with default users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails3BootstrapDeviseCancan::Application.routes.draw do
   root :to => "home#index"
 
   devise_for :users
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
