6d1a82520626efdfe2bd0ece0c92451c8b6d063c
Martin Burke <mahtyb@gmail.com>
2013-06-14 11:33:17 -0700

    Set up each new user with a default role of USER

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,14 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  before_create :assign_role
   
   validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
+  
+  def assign_role
+    # assign a default role if no role is assigned
+    self.add_role :user if self.roles.first.nil?
+  end
   
 end
