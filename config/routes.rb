09dcd00e591804663c69a0ebfc688fe2acc6db68
Martin Burke <mahtyb@gmail.com>
2013-06-13 17:02:47 -0700

    Add Admin Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
+  get "users/index"
+
+  get "users/show"
+
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
-
   devise_for :users
   resources :users
 
